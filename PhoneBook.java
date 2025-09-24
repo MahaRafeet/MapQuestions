cb3d1e268f4d88f6bddc8d86070e49e5df0e1e3f
Maha <mahathamood98@gmail.com>
2025-09-24 15:05:35 +0400

    Adding question 5 solution

---
+++ PhoneBook.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class PhoneBook {
+    private Map<String, String> contacts = new HashMap<>();
+
+    public PhoneBook() {
+        contacts.put("Alice", "555-1111");
+        contacts.put("Bob", "555-2222");
+    }
+
+    public static void main(String[] args) {
+        PhoneBook phoneBook=new PhoneBook();
+        System.out.println(phoneBook.removeContact("Alice","555-1111"));
+        System.out.println(phoneBook.removeContact("Bob","000-0000"));
+        System.out.println(phoneBook.getContacts());
+
+    }
+
+    public  boolean removeContact(String name, String number) {
+        return contacts.remove(name, number);
+    }
+
+    public  Map<String, String> getContacts() {
+        return contacts;
+    }
+}
